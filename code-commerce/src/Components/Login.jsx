cb067fbbefb5a02f680a6ed5614fca35fa81c0de
Nick Hoang <nickjhoang@gmail.com>
2023-02-14 13:53:30 -0600

    work on validation to ensure that user inputs correct type of input

--- code-commerce/src/Components/Login.jsx
+++ code-commerce/src/Components/Login.jsx
@@ -9,26 +9,47 @@ export default class extends React.Component {
         }
     }
 
-
-// handleSubmit = () => {}
-// handleInput = () => {}
+handleSubmit = (event) => {
+    event.preventDefault();
+      
+    const form = event.target;
+    const formElements = form.elements;
+      
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+    
+        if (element.hasAttribute('required') && element.value === '') {
+        element.setCustomValidity(`${element.name} is required`);
+        } else {
+        element.setCustomValidity('');
+        }
+    }
+      
+    if (form.checkValidity()) {
+          // Call the submit function here
+    }
+}
+      
+handleInput = (event) => {
+    const input = event.target;
+}
 
     render() {
 
         const signIn = this.state.signIn;
 
         const fields = [
-            {label: 'Your Email Address *', name: 'email', required: 'true', typeInput: 'email'},
-            {label: 'Create Password *', name: 'password', required: 'true', typeInput: 'text'},
-            {label: 'Confirm Password *', name: 'confirmPassword', required: 'true', typeInput: 'text'},
-            {label: 'First Name *', name: 'name', required: 'true', typeInput: 'text'},
-            {label: 'Surname *', name: 'surname', required: 'true', typeInput: 'text'},
-            {label: 'Postcode', name: 'postalCode', required: 'false', typeInput: 'number'}
+            {label: 'Your Email Address *', name: 'email', required: true, typeInput: 'email', errorMessage: 'Please enter a valid email'},
+            {label: 'Create Password *', name: 'password', required: true, typeInput: 'password', pattern:"^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$", errorMessage: 'Please enter a valid password'},
+            {label: 'Confirm Password *', name: 'confirmPassword', required: true, typeInput: 'password', errorMessage: 'Your passwords do not match'},
+            {label: 'First Name *', name: 'name', required: true, typeInput: 'text', pattern: "^[A-Za-z0-9]{3,16}$", errorMessage: 'Name should be 3-16 characters and should not include any special characters'},
+            {label: 'Surname *', name: 'surname', required: true, typeInput: 'text', pattern: "^[A-Za-z0-9]{3,16}$", errorMessage: 'Name should be 3-16 characters and should not include any special characters'},
+            {label: 'Postcode', name: 'postalCode', required: true, typeInput: 'number'}
         ]
 
         const signInFields = [
-            {label: 'Email Address *', name: 'email', required: 'true', typeInput: 'email'},
-            {label: 'Password *', name: 'password', required: 'true', typeInput: 'text'},
+            {label: 'Email Address *', name: 'email', required: 'true', typeInput: 'email', errorMessage: 'Name should be 3-16 characters and should not include any special characters'},
+            {label: 'Password *', name: 'password', required: 'true', typeInput: 'password', errorMessage: 'Please enter a valid password',pattern:"^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$"}
         ]
 
         return (
@@ -40,11 +61,32 @@ export default class extends React.Component {
                 <input type="radio" id="create-account" name="toggle" value="sign-up" checked={!signIn} onChange={() => this.setState({ signIn: false })} />
                 <label htmlFor="sign-up">CREATE ACCOUNT</label>
             </div>
-
-                <form>
-                {!signIn ? (
-                    fields.map((field, index) => {
-                        const {label, name, required, typeInput} = field;
+            <form>
+                {!signIn ? (fields.map((field, index) => {
+                    const {label, name, required, typeInput, errorMessage, pattern} = field;
+                    return (
+                        <>
+                        <div className='FormItem'>
+                            <label> {label} </label>
+                            <div className='FormInput'>
+                            <input 
+                                name={name}
+                                onChange={this.handleInput}
+                                autoComplete="off"
+                                type={typeInput}
+                                required={required}
+                                pattern={pattern}
+                            />
+                            <span className='ErrorMessageText'>{errorMessage}</span>
+                            </div>
+                        </div>
+                        {index === 1 && (
+                            <div className='PasswordDisclaimer'>Password must be 8-20 characters, including at least one capital letter, at least one small letter, one number and one special character -!@#$%^&*()_+</div>
+                        )}
+                        </>
+                    )
+                })) : ( signInFields.map((signInFields, index) => {
+                        const {label, name, required, typeInput, errorMessage, pattern} = signInFields;
                         return (
                             <>
                             <div className='FormItem'>
@@ -56,40 +98,19 @@ export default class extends React.Component {
                                     autoComplete="off"
                                     type={typeInput}
                                     required={required}
+                                    pattern={pattern}
                                 />
+                                <span className='ErrorMessageText'>{errorMessage}</span>
                                 </div>
                             </div>
-                            {index === 1 && (
-                                <div className='PasswordDisclaimer'>Password must be 8-20 characters, including at least one capital letter, at least one small letter, one number and one special character -!@#$%^&*()_+</div>
-                            )}
                             </>
-                        )
-                    })
-                ) : (
-                        signInFields.map((signInFields, index) => {
-                            const {label, name, required, typeInput} = signInFields;
-                            return (
-                                <>
-                                <div className='FormItem'>
-                                    <label> {label} </label>
-                                    <div className='FormInput'>
-                                    <input 
-                                        name={name}
-                                        onChange={this.handleInput}
-                                        autoComplete="off"
-                                        type={typeInput}
-                                        required={required}
-                                    />
-                                    </div>
-                                </div>
-                                </>
                             )
                         })
                     )}
                 </form>
                 <div className='BottomInfo'>
                     <button
-                        onSubmit='handleSubmit'
+                        onSubmit={this.handleSubmit}
                         className='SaveButton'>SAVE</button>
                     <br />
                     <hr style={{ width: '40%', display: 'inline-block' }} />
