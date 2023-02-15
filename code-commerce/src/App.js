f0532e2bbb2aeffb59b9506edd5e2e21d67f3817
Nick Hoang <nickjhoang@gmail.com>
2023-02-15 12:01:44 -0600

    finish functionality of initial sign-in / create account form with proper error messages only appearing after the 'save' button is clicked

--- code-commerce/src/App.js
+++ code-commerce/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from "./Components/Login.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const INIT_STATE = {
   cartTotal: 0,
 }
-
-
 
 function App() {
   return (
--- code-commerce/src/Components/Login.jsx
+++ code-commerce/src/Components/Login.jsx
@@ -1,55 +1,54 @@
 import styles from '../App.css';
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEye} from '@fortawesome/free-solid-svg-icons';
 
 export default class extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             signIn: true,
+            submitted: false,
         }
     }
 
 handleSubmit = (event) => {
+    console.log('pressed');
     event.preventDefault();
       
     const form = event.target;
     const formElements = form.elements;
       
-    for (let i = 0; i < formElements.length; i++) {
-        const element = formElements[i];
-    
-        if (element.hasAttribute('required') && element.value === '') {
-        element.setCustomValidity(`${element.name} is required`);
-        } else {
-        element.setCustomValidity('');
-        }
-    }
-      
     if (form.checkValidity()) {
-          // Call the submit function here
+        this.setState({ submitted: true });
     }
+
 }
       
 handleInput = (event) => {
     const input = event.target;
-}
+    this.setState((prevState) => ({
+      [input.name]: input.checkValidity(),
+    }));
+  }
 
     render() {
 
         const signIn = this.state.signIn;
+        const submitted = this.state.submitted;
 
         const fields = [
             {label: 'Your Email Address *', name: 'email', required: true, typeInput: 'email', errorMessage: 'Please enter a valid email'},
             {label: 'Create Password *', name: 'password', required: true, typeInput: 'password', pattern:"^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$", errorMessage: 'Please enter a valid password'},
             {label: 'Confirm Password *', name: 'confirmPassword', required: true, typeInput: 'password', errorMessage: 'Your passwords do not match'},
-            {label: 'First Name *', name: 'name', required: true, typeInput: 'text', pattern: "^[a-zA-Z]+$", errorMessage: 'Name should not include any special characters or numbers'},
-            {label: 'Surname *', name: 'surname', required: true, typeInput: 'text', pattern: "^[A-Za-z]+$", errorMessage: 'Name should not include any special characters or numbers'},
+            {label: 'First Name *', name: 'name', required: true, typeInput: 'text', pattern: "^[a-zA-Z]+$", errorMessage: 'Please enter a valid First Name'},
+            {label: 'Surname *', name: 'surname', required: true, typeInput: 'text', pattern: "^[A-Za-z]+$", errorMessage: 'Please enter a valid  Surname'},
             {label: 'Postcode', name: 'postalCode', required: true, typeInput: 'number'}
         ]
 
         const signInFields = [
-            {label: 'Email Address *', name: 'email', required: 'true', typeInput: 'email', errorMessage: 'Name should be 3-16 characters and should not include any special characters'},
-            {label: 'Password *', name: 'password', required: 'true', typeInput: 'password', errorMessage: 'Please enter a valid password',pattern:"^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$"}
+            {label: 'Email Address *', name: 'email', required: true, typeInput: 'email', errorMessage: 'Please enter a valid email'},
+            {label: 'Password *', name: 'password', required: true, typeInput: 'password', errorMessage: 'Please enter a valid password',pattern:"^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$"}
         ]
 
         return (
@@ -61,7 +60,7 @@ handleInput = (event) => {
                 <input type="radio" id="create-account" name="toggle" value="sign-up" checked={!signIn} onChange={() => this.setState({ signIn: false })} />
                 <label htmlFor="sign-up">CREATE ACCOUNT</label>
             </div>
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 {!signIn ? (fields.map((field, index) => {
                     const {label, name, required, typeInput, errorMessage, pattern} = field;
                     return (
@@ -77,7 +76,9 @@ handleInput = (event) => {
                                 required={required}
                                 pattern={pattern}
                             />
-                            <span className='ErrorMessageText'>{errorMessage}</span>
+                            {submitted && !this.state[name] && (
+                                <span className='ErrorMessageText'>{errorMessage}</span>
+                            )}
                             </div>
                         </div>
                         {index === 1 && (
@@ -100,7 +101,9 @@ handleInput = (event) => {
                                     required={required}
                                     pattern={pattern}
                                 />
+                            {submitted && !this.state[name] && (
                                 <span className='ErrorMessageText'>{errorMessage}</span>
+                            )}
                                 </div>
                             </div>
                             </>
@@ -110,7 +113,7 @@ handleInput = (event) => {
                 </form>
                 <div className='BottomInfo'>
                     <button
-                        onSubmit={this.handleSubmit}
+                        onClick={this.handleSubmit}
                         className='SaveButton'>SAVE</button>
                     <br />
                     <hr style={{ width: '40%', display: 'inline-block' }} />
